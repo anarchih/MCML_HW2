88cdfdce7b84aef3f3b01f42d29a3ae572fcf8ed
benwu95 <benwu95@gmail.com>
2016-11-12 16:38:40 +0800

    add SVM kernel types and feature scaling methods

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 MCML_HW2
 ==============
 
-python3 main.py training/ test/
+```
+python3 main.py training/ test/ [model_method] [kernel_type] [feature_scaling]
+```
+
+##### model_method
+- 1: create 26 two-class classifiers
+- 2: use the default model
+
+##### kernel_type
+- linear
+- poly
+- rbf
+- sigmoid
+
+##### feature_scaling
+- 1: rescaling the range in [0, 1]
+- 2: standardization
+- 3: normalization (L2 norm)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVC
 from sklearn import decomposition, preprocessing
 import sys
 from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import classification_report
 
 
 class ImageItem(object):
@@ -50,36 +51,71 @@ def main():
     num_class = 26
     training_directory = sys.argv[1]
     test_directory = sys.argv[2]
+    model_method = sys.argv[3]
+    kernel_type = sys.argc[4]
+    feature_scaling = sys.argc[5]
 
     # Read, Preprocess and Downsize Image
     images, labels = readfiles(training_directory, img_preprocess=method_1)
     test_images, test_labels = readfiles(test_directory, img_preprocess=method_1)
 
+    # Feature scaling
+    # Rescaling the range in [0, 1]
+    if feature_scaling == 1:
+        images = preprocessing.minmax_scale(images)
+        test_images = preprocessing.minmax_scale(test_images)
     # Standardization
-    images = preprocessing.scale(images)
-    test_images = preprocessing.scale(test_images)
+    if feature_scaling == 2:
+        images = preprocessing.scale(images)
+        test_images = preprocessing.scale(test_images)
+    # Normalization (L2 norm)
+    if feature_scaling == 3:
+        images = preprocessing.normalize(images)
+        test_images = preprocessing.normalize(test_images)
 
     # Feature Selection
-    images, test_images = pca_transform(images, test_images, 6)
+    images, test_images = pca_transform(images, test_images, 3)
 
-    # Training
-    tmp_label = [0] * len(labels)
-    model = [SVC() for i in range(num_class)]
-    for i in range(num_class):
-        for j in range(len(labels)):
-            tmp_label[j] = 1 if i == labels[j] else 0
-        model[i].fit(images, tmp_label)
+    # Training and Testing
+    # create 26 two-class classifiers
+    if model_method == 1:
+        # Training
+        tmp_label = [0] * len(labels)
+        model = [SVC(kernel=kernel_type) for i in range(num_class)]
+        for i in range(num_class):
+            for j in range(len(labels)):
+                tmp_label[j] = 1 if i == labels[j] else 0
+            model[i].fit(images, tmp_label)
 
-    # Testing
-    tmp_label = [0] * len(test_labels)
-    print("label  precision   recall")
-    for i in range(num_class):
-        for j in range(len(test_labels)):
-            tmp_label[j] = 1 if i == test_labels[j] else 0
-        test_pred = model[i].predict(test_images)
-        p = precision_score(tmp_label, test_pred)
-        r = recall_score(tmp_label, test_pred)
-        print("    %c     %.4f   %.4f" % (chr(i + 65), p, r))
+        # Testing
+        tmp_label = [0] * len(test_labels)
+        avg_p = 0
+        avg_r = 0
+        print("label  precision   recall")
+        for i in range(num_class):
+            for j in range(len(test_labels)):
+                tmp_label[j] = 1 if i == test_labels[j] else 0
+            test_pred = model[i].predict(test_images)
+            p = precision_score(tmp_label, test_pred)
+            r = recall_score(tmp_label, test_pred)
+            avg_p += p
+            avg_r += r
+            print("    %c     %.4f   %.4f" % (chr(i + 65), p, r))
+        avg_p /= 26
+        avg_r /= 26
+        print("-------------------------")
+        print("  %s     %.4f   %.4f" % "AVG", avg_p, avg_r)
+
+    # use the default model
+    if model_method == 2:
+        # Training
+        model = SVC(kernel=kernel_type)
+        model.fit(images, labels)
+
+        # Testing
+        test_pred = model.predict(test_images)
+        names = [chr(i + 65) for i in range(num_class)]
+        print(classification_report(test_labels, test_pred, target_names=names))
 
 
 if __name__ == "__main__":
